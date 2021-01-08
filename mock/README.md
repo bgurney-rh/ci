5b4e8b1e9bb32739b09117c6ac7391d784be1fdc
Bryan Gurney <bgurney@redhat.com>
2021-01-08 17:08:42 -0500

    create stratis-mock-run script set with unified stratisd.spec
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

---
+++ mock/README.md
@@ -0,0 +1,3 @@
+# Stratis mock test run script
+
+The stratis-mock-run.sh script builds a set of testing RPM packages of the stratisd and stratis-cli default branches, using mock.
---
+++ mock/stratis-mock-run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+export STRATIS_DBUS_TIMEOUT=300000
+
+if [ ! -e /etc/stratis/test_config.json ]
+then
+	echo "No test device config found; create a test_config.json"
+	echo "file in /etc/stratis with three devices that can be"
+	echo "overwritten for testing."
+	exit 8
+fi
+
+# Create an array of test devices from the test_config.json file.
+# This is a naive search for device paths starting with "/dev", then
+# stripping out the quote and comma characters.
+TESTDEVS_RESULT=$(./parse_json.py /etc/stratis/test_config.json)
+TESTDEVS=($(echo $TESTDEVS_RESULT | tr ',' ' '))
+
+if [ ! -e stratisd.spec -o ! -e stratis-cli.spec ]
+then
+	echo "Both stratisd.spec and stratis-cli.spec must be present."
+	exit 4
+fi
+
+BASEDEPPKGS=(
+	python3-dbus-client-gen
+	python3-dbus-python-client-gen
+	python3-justbytes
+	python3-psutil
+	rpm-build
+	rpmdevtools
+	keyutils
+	asciidoc
+	python3-devel
+	dbus-devel
+	systemd-devel
+	rust
+	cargo
+	openssl-devel
+	clang
+	llvm
+	llvm-devel
+	cryptsetup-devel
+	libblkid-devel
+	make
+)
+
+dnf -y install "${BASEDEPPKGS[@]}"
+
+STRATISD_N=$(rpmspec -P stratisd.spec | grep ^Name | awk {'print $2'})
+STRATISD_V=$(rpmspec -P stratisd.spec | grep ^Version | awk {'print $2'})
+STRATISD_R=$(rpmspec -P stratisd.spec | grep ^Release | awk {'print $2'})
+STRATISD_RPMBASENAME="${STRATISD_N}-${STRATISD_V}-${STRATISD_R}"
+echo "stratisd package target: $STRATISD_RPMBASENAME"
+STRATIS_CLI_N=$(rpmspec -P stratis-cli.spec | grep ^Name | awk {'print $2'})
+STRATIS_CLI_V=$(rpmspec -P stratis-cli.spec | grep ^Version | awk {'print $2'})
+STRATIS_CLI_R=$(rpmspec -P stratis-cli.spec | grep ^Release | awk {'print $2'})
+STRATIS_CLI_RPMBASENAME="${STRATIS_CLI_N}-${STRATIS_CLI_V}-${STRATIS_CLI_R}"
+echo "stratis-cli package target: $STRATIS_CLI_RPMBASENAME"
+
+./reset-upstream-stratis-repos.sh
+
+echo "Starting build and package process..."
+./build-and-package.sh
